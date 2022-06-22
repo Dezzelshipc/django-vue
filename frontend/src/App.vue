6854cf56b340436e78895b16394e0e489a45c984
Andrey-Rudenko <62091872+Andrey-Rudenko@users.noreply.github.com>
2022-06-22 14:55:07 +1000

    animated background demo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="content">
-    <router-view v-if="isLogin && !dev"></router-view>
-    <div v-else>
-      <header>
-        <TabMenu :model="items" />
-      </header>
-      <router-view></router-view>
+  <v-vanta effect="fog" :options="options"></v-vanta>
+  <div class="boddy" id="boddy">
+    <TabMenu :model="items" />
+    <div class="content">
+      <router-view v-if="isLogin && !dev"></router-view>
+      <div v-else>
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import TabMenu from 'primevue/tabmenu';
+import TabMenu from 'primevue/tabmenu'
+import VVanta from 'vue-vanta';
 
 
 export default {
   components: {
     TabMenu,
+    VVanta,
   },
   name: 'App',
   title() {
@@ -24,6 +27,18 @@ export default {
   },
   data() {
     return {
+      options: {
+        el: "#boddy",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 1000.00,
+        minWidth: 200.00,
+        highlightColor: 0x96ff,
+        midtoneColor: 0xa400ff,
+        lowlightColor: 0x5b4f9d,
+        baseColor: 0xffffff,
+      },
       check: false,
       items: [
         {label: 'Home', icon: 'pi pi-fw pi-home', to: '/home' },
@@ -66,10 +81,10 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-body {
-  background-image: url('https://images.saymedia-content.com/.image/t_share/MTc4NzM1OTc4MzE0MzQzOTM1/how-to-create-cool-website-backgrounds-the-ultimate-guide.png');
-  background-size: 100%;
-}
+/*body {*/
+/*  background-image: url('https://images.saymedia-content.com/.image/t_share/MTc4NzM1OTc4MzE0MzQzOTM1/how-to-create-cool-website-backgrounds-the-ultimate-guide.png');*/
+/*  background-size: 100%;*/
+/*}*/
 .content {
   width: 1000px;
   min-height: 300px;
@@ -79,7 +94,7 @@ body {
   margin-top: 100px;
   margin-bottom: 100px;
   background-color: rgba(255, 255, 255, .25);
-  backdrop-filter: blur(5px);
+  backdrop-filter: blur(8px);
   padding: 50px;
 }
 .p-tabmenu .p-tabmenu-nav{
