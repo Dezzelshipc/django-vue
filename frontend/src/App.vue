6c1608fcc4c940930672349795fe51ce05013f51
Dezzelshipc <foler126@gmail.com>
2022-07-01 11:17:38 +1000

    2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -153,11 +153,12 @@ nav {
   justify-content: center;
 }
 .all {
-  height: max(100%, 100vh);
+  height: 100%;
 }
 body {
   background-color: #BBE4F1;
   margin: 0;
   padding: 0;
+  height: max(100%, 100vh);
 }
 </style>
